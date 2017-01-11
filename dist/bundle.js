df93632adfb8e91ae1edbd9ae127884c26b71c0e
Atishay Baid <atishaybaid@Atishays-MacBook-Air.local>
2017-01-12 01:46:45 +0530

    converted to extension,working fine in new tab

--- dist/bundle.js
+++ dist/bundle.js
@@ -64,68 +64,51 @@
 
 	(function () {
 
-	  document.addEventListener('DOMContentLoaded', init);
-	  /*window.addEventListener('error', function (e) {
-	  	console.log("error handler called");
-	      var error = e.error;
-	      console.log(error);
-	  });*/
+		document.addEventListener('DOMContentLoaded', init);
 
-	  window.onerror = function (msg, url, line, col, error) {
-	    console.log(msg);
-	    // Note that col & error are new to the HTML 5 spec and may not be 
-	    // supported in every browser.  It worked for me in Chrome.
-	    //var extra = !col ? '' : '\ncolumn: ' + col;
-	    //extra += !error ? '' : '\nerror: ' + error;
+		window.onerror = function (msg, url, line, col, error) {
+			console.log(msg);
+			var suppressErrorAlert = true;
 
-	    // You can view the information in an alert to see things working like this:
-	    //alert("Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
+			return suppressErrorAlert;
+		};
 
-	    // TODO: Report this error via ajax so you can keep track
-	    //       of what pages have JS issues
+		function init() {
+			var inputTextarea = document.getElementsByClassName('es5-input')[0];
+			var outputTextarea = document.getElementsByClassName('es6-input')[0];
 
-	    var suppressErrorAlert = true;
-	    // If you return true, then error alerts (like in older versions of 
-	    // Internet Explorer) will be suppressed.
-	    return suppressErrorAlert;
-	  };
+			var _Editor$create = _editor2.default.create(inputTextarea, outputTextarea);
 
-	  function init() {
-	    var inputTextarea = document.getElementsByClassName('es5-input')[0];
-	    var outputTextarea = document.getElementsByClassName('es6-input')[0];
+			var _Editor$create2 = _slicedToArray(_Editor$create, 2);
 
-	    var _Editor$create = _editor2.default.create(inputTextarea, outputTextarea);
+			inputEditor = _Editor$create2[0];
+			outputEditor = _Editor$create2[1];
 
-	    var _Editor$create2 = _slicedToArray(_Editor$create, 2);
+			inputEditor.on("change", transform);
 
-	    inputEditor = _Editor$create2[0];
-	    outputEditor = _Editor$create2[1];
+			getLocalStorageData(inputEditor);
+		}
 
-	    inputEditor.on("change", transform);
+		function transform(item, changeObj) {
 
-	    getLocalStorageData(inputEditor);
-	  }
+			var compiler = new _compiler2.default(window.Babel);
 
-	  function transform(item, changeObj) {
+			var _compiler$compile = compiler.compile(inputEditor.getValue()),
+			    _compiler$compile2 = _slicedToArray(_compiler$compile, 2),
+			    output = _compiler$compile2[0],
+			    error = _compiler$compile2[1];
 
-	    var compiler = new _compiler2.default(window.Babel);
+			;
+			outputEditor.setValue(output);
+			window.localStorage.setItem("es5Code", inputEditor.getValue());
+		}
 
-	    var _compiler$compile = compiler.compile(inputEditor.getValue()),
-	        _compiler$compile2 = _slicedToArray(_compiler$compile, 2),
-	        output = _compiler$compile2[0],
-	        error = _compiler$compile2[1];
-
-	    ;
-	    outputEditor.setValue(output);
-	    window.localStorage.setItem("es5Code", inputEditor.getValue());
-	  }
-
-	  function getLocalStorageData(inputEditor) {
-	    var localStorageData = window.localStorage.getItem('es5Code');
-	    if (localStorageData) {
-	      inputEditor.setValue(localStorageData);
-	    }
-	  }
+		function getLocalStorageData(inputEditor) {
+			var localStorageData = window.localStorage.getItem('es5Code');
+			if (localStorageData) {
+				inputEditor.setValue(localStorageData);
+			}
+		}
 	})();
 
 /***/ },
